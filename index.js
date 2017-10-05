68a05d88f54a6d9200c9104441e420ce3b4d1aee
Halfdan Hem <halfdanhem@gmail.com>
2017-10-05 12:59:35 -0700

    add correct headers to response

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const rp = require('request-promise');
 const API_KEY = process.env.TRANSLINK_API_ID;
 
-function handler(message) {
+function handler() {
+  return requestBusesFromTranslink().then(transformTranslinkResponse);
+}
+
+function requestBusesFromTranslink() {
   const getBusesRequest = {
     uri: `http://api.translink.ca/rttiapi/v1/buses\?apikey\=${API_KEY}`,
     headers: { 'Accept': 'application/JSON' },
     json: true,
+    method: 'GET',
   }
 
   return rp(getBusesRequest);
 }
 
+function transformTranslinkResponse(response) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: response,
+  }
+}
+
 module.exports = handler;
+
+// Debugging code
+// handler().then((response) => {
+//   console.log(response);
+// })
